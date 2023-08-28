9726f5e8b0f285f6dbab07a7a45d9b4707f41daa
Julie Foster <juliefoster6695@gmail.com>
2023-08-28 15:07:23 -0400

    added emergency and show all buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 export const App = () => {
   const [allTickets, setAllTickets] = useState([]);
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
+  const [filteredTickets, setFilteredTickets] = useState();
 
   useEffect(() => {
     getAllTickets().then((ticketsArray) => {
@@ -12,9 +14,38 @@ export const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
+    } else {
+      setFilteredTickets(allTickets);
+    }
+  }, [showEmergencyOnly]);
+
   return (
     <div className="tickets-container">
       <h2>Tickets</h2>
+      <div>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(true);
+          }}
+        >
+          Emergency
+        </button>
+        <button
+          className="filter-btn btn-info"
+          onClick={() => {
+            setShowEmergencyOnly(false);
+          }}
+        >
+          Show All
+        </button>
+      </div>
       <article className="tickets">
         {allTickets.map((ticket) => {
           return (
